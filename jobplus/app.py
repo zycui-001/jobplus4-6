999b039ea35e26f6e42e3de30d0a4d44a008ef63
564480667 <564480667@qq.com>
2018-02-24 16:41:41 +0800

    changge file 2-24

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from jobplus.config import configs
-from jobplus.models import db, Course
+from jobplus.models import db
+from flask_migrate import Migrate 
 
 def register_blueprints(app):
 	from .handlers import front
@@ -11,5 +12,6 @@ def create_app(config):
 	app = Flask(__name__)
 	app.config.from_object(configs.get(config))
 	db.init_app(app)
+	Migrate(app, db)
 	register_blueprints(app)
-    return app
+	return app
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from jobplus.app import create_app
 
 # use productionconfig
 
-app = create_app(production)
+app = create_app('production')
 
 if __name__ == '__main__':
 	app.run()
